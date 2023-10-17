5ea7cdd0a16c8714bd791cdd206f67075e7f90a4
Mustafa-pc <vild.mustafin2017@yandex.ru>
2023-10-17 16:56:16 +0500

    Добавьте файлы проекта.

---
+++ RazorPages/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPages.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Display(Name = "Название")]
+        public string? Title { get; set; } = string.Empty;
+        [Display(Name = "Дата выхода")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Display(Name = "Жанр")]
+        public string? Genre { get; set; }
+        [Display(Name = "Цены в Омарах"), Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public string Rating { get; set; } =string.Empty;
+
+    }
+}
---
+++ RazorPages/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Data;
+using RazorPages.Models;
+
+namespace RazorPages.Models;
+
+public class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using(var context = new RazorPagesContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<RazorPagesContext>>()))
+        {
+            if (context == null|| context.Movie==null) 
+            {
+                throw new ArgumentNullException("Null RazorPagesContext");
+            }
+
+            //Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;
+            }
+
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M,
+                    Rating = "3/5"
+                }, 
+                new Movie
+                {
+                Title = "Амир Топ 1 Взял",
+                    ReleaseDate = DateTime.Parse("2023-10-14"),
+                    Genre = "Action Triller",
+                    Price = 4.3M,
+                    Rating = "5/5"
+                },
+                new Movie
+                {
+                    Title = "Перевёртыш",
+                    ReleaseDate = DateTime.Parse("2023-10-12"),
+                    Genre = "Action Triller",
+                    Price = 0.1M,
+                    Rating = "5/5"
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1998-10-12"),
+                    Genre = "Action Triller",
+                    Price = 80M,
+                    Rating = "4/5"
+                }
+            );
+
+            context.SaveChanges(); 
+        }
+    }
+}
