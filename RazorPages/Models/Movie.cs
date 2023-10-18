b2127fc1457b1597cd670de5a6eac9599e078c70
Mustafa-pc <vild.mustafin2017@yandex.ru>
2023-10-18 15:31:25 +0500

    RpagesLessDone

--- RazorPages/Models/Movie.cs
+++ RazorPages/Models/Movie.cs
@@ -6,15 +6,36 @@ namespace RazorPages.Models
     public class Movie
     {
         public int Id { get; set; }
-        [Display(Name = "Название")]
+
+        [Display(Name = "Название"),StringLength(60,MinimumLength =3)]
+        [Required]
         public string? Title { get; set; } = string.Empty;
+
+
+
         [Display(Name = "Дата выхода")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        [Display(Name = "Жанр")]
-        public string? Genre { get; set; }
-        [Display(Name = "Цена"), Column(TypeName = "decimal(18,2)")]
+
+
+        [Display(Name = "Цена"), Range(10,2000)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+
+        [Display(Name = "Жанр")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+
+
+        [Display(Name = "Возрастной рейтинг")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; } =string.Empty;
 
     }
